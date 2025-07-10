a72f2a018ee62ca7d2b72a11610a3f0313110be7
Leonardo Pulgar <150628815+Leryons@users.noreply.github.com>
2025-07-10 13:50:48 -0400

    Main Commit

---
+++ Model/User.cs
@@ -0,0 +1,11 @@
+namespace Hashing_Guard.Model
+{
+    public class User
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public string Role { get; set; } = "User";
+    }
+}
